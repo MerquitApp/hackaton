1d24446a4a64fe8b6a2d52c86acb7d85d12855cb
Hugo Boyano <hnboyano@gmail.com>
2024-10-31 11:01:03 +0100

    add fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import GestureGame from "./components/JuegoImagenes";
-import Imagen from "./components/Imagen";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GestureGame from './components/JuegoImagenes';
+import Imagen from './components/Imagen';
 
 function App() {
   return (
     <div>
       <Router>
-      <Routes>
-        <Route path="/" element={<Imagen />} />
-        <Route path="/juego" element={<GestureGame />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Imagen />} />
+          <Route path="/juego" element={<GestureGame />} />
+        </Routes>
+      </Router>
     </div>
   );
+}
 
 export default App;
